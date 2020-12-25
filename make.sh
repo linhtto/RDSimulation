19a578f935d57c9b30fce508760d119cb8236264
matthew-hong <hongmatt@bu.edu>
2020-12-25 14:57:05 -0500

    ASST-40: Change code so that all files in output/ is deleted instead of one by one.

--- make.sh
+++ make.sh
@@ -8,29 +8,10 @@ else
 fi
 
 # Remove existing RMarkdowns before re-compiling new versions
-if [ -e /output/rd_exercise_base_zero.html ]
-then
-	rm output/rd_exercise_base_zero.html
-fi
-
-if [ -e output/rd_exercise_base_bwo.html ]
-then
-	rm output/rd_exercise_base_bwo.html
-fi
-
-if [ -e output/rd_exercise_stressout_bwo.html ]
-then
-	rm output/rd_exercise_stressout_bwo.html
-fi
-
-if [ -e output/rd_exercise_stressout_zero.html ]
-then
-	rm output/rd_exercise_stressout_zero.html
-fi
-
-if [ -e output/rd_exercise_intervals.html ]
-then
-	rm output/rd_exercise_intervals.html
+if [ -z "$(ls output/)" ]
+then echo -e "\n\n output directory is empty, nothing to delete \n\n"
+else 
+	rm output/*
 fi
 
 # Actually compile RMarkdowns for two parameters ()
