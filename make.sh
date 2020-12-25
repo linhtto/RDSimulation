87078a4cdd5a3786e3c448aa13cf2604338fdbd1
matthew-hong <hongmatt@bu.edu>
2020-12-25 17:03:19 -0500

    ASST-40: Use -n instead of -z

--- make.sh
+++ make.sh
@@ -1,14 +1,14 @@
 #!/bin/env Rscript
 
 # Clear out figures directory before compiling RMarkdown
-if [ -z "$(ls figures/)" ]
+if [ ! -n "$(ls figures/)" ]
 then echo -e "\n\n figures directory is empty, nothing to delete \n\n"
 else 
 	rm figures/*
 fi
 
 # Remove existing RMarkdowns before re-compiling new versions
-if [ -z "$(ls output/)" ]
+if [ ! -n "$(ls output/)" ]
 then echo -e "\n\n output directory is empty, nothing to delete \n\n"
 else 
 	rm output/*
@@ -20,10 +20,10 @@ output_format = 'html_document', output_dir = 'output/',\
 output_file ='rd_exercise_base_bwo',\
 params=list(exercise = 'bwo'))"
 
-Rscript -e "rmarkdown::render('code/rd_exercise_base.rmd',\
-output_format = 'html_document', output_dir = 'output/',\
-output_file ='rd_exercise_base_zero',\
-params=list(exercise = 'zero'))"
+# Rscript -e "rmarkdown::render('code/rd_exercise_base.rmd',\
+# output_format = 'html_document', output_dir = 'output/',\
+# output_file ='rd_exercise_base_zero',\
+# params=list(exercise = 'zero'))"
 
 # Rscript -e "rmarkdown::render('code/rd_exercise_stressout.rmd',\
 # output_format = 'html_document', output_dir = 'output/',\
@@ -37,10 +37,4 @@ params=list(exercise = 'zero'))"
 
 # Rscript -e "rmarkdown::render('code/rd_exercise_intervals.rmd',\
 # output_format = 'html_document', output_dir = 'output/',\
-# output_file ='rd_exercise_intervals_bwo',\
-# params=list(exercise = 'bwo'))"
-
-# Rscript -e "rmarkdown::render('code/rd_exercise_intervals.rmd',\
-# output_format = 'html_document', output_dir = 'output/',\
-# output_file ='rd_exercise_intervals_zero',\
-# params=list(exercise = 'zero'))"
+# output_file ='rd_exercise_intervals')"
